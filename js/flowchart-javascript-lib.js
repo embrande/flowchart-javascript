5945943a50210091ae2850432fcc395e5bc0da83
U-ADS\embrande <embrande@IN-STSV-25JNJB2.ads.iu.edu>
2019-05-01 16:55:26 -0400

    initial setup

---
+++ js/flowchart-javascript-lib.js
@@ -0,0 +1,29 @@
+// expects a canvas with a flowchart-stage id
+// expects a object call after it with JSON and the following layout:
+/*
+
+
+	flowchart({
+	
+	});
+
+
+
+*/
+
+var flowchartAppController = function(){
+
+};
+
+
+
+var flowchartUIController = function(){
+
+};
+
+
+
+
+var flowchartDataController = function(){
+
+};
