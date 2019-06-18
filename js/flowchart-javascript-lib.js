08d20219e4bec4f324d790f2f1934332e67406f1
U-ADS\embrande <embrande@IN-STSV-25JNJB2.ads.iu.edu>
2019-06-18 13:25:54 -0400

    added dom ready support

--- js/flowchart-javascript-lib.js
+++ js/flowchart-javascript-lib.js
@@ -956,5 +956,43 @@ var flowchartAppController = (function(dCon, UICon){
 
 
 
-flowchartAppController.init("flowchart-stage", flowchartStage);
-flowchartAppController.createMenu('top');
+
+// dom ready support
+var domIsReady = (function(domIsReady) {
+   var isBrowserIeOrNot = function() {
+      return (!document.attachEvent || typeof document.attachEvent === "undefined" ? 'not-ie' : 'ie');
+   }
+
+   domIsReady = function(callback) {
+      if(callback && typeof callback === 'function'){
+         if(isBrowserIeOrNot() !== 'ie') {
+            document.addEventListener("DOMContentLoaded", function() {
+               return callback();
+            });
+         } else {
+            document.attachEvent("onreadystatechange", function() {
+               if(document.readyState === "complete") {
+                  return callback();
+               }
+            });
+         }
+      } else {
+         console.error('The callback is not a function!');
+      }
+   }
+
+   return domIsReady;
+})(domIsReady || {});
+
+(function(document, window, domIsReady, undefined) {
+   domIsReady(function() {
+   		/*
+   			*****
+   				Main function call
+   			*****
+   		*/
+		flowchartAppController.init("flowchart-stage", flowchartStage);
+		flowchartAppController.createMenu('top');
+   });
+})(document, window, domIsReady);
+
